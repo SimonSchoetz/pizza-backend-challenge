024cee70165284441d89116cbad3fa83ec1b7c11
Simon <simonsch.tz@gmail.com>
2019-12-18 16:10:35 +0100

    Add Dropdown Placeholder and space for final list

--- configuration.js
+++ configuration.js
@@ -1,1 +1,4 @@
 /**Script to configurate one's Pizza*/
+
+// Each button passes it's content (word) into a function call
+// So there is only one function to actually pass the ingredients to the list (for each category)
