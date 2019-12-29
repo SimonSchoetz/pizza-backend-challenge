0d4141da202ad1bd10588c049b28092670d03eee
Simon <simonsch.tz@gmail.com>
2019-12-29 20:08:56 +0100

    set up ingredients in js

--- configuration.js
+++ configuration.js
@@ -2,3 +2,24 @@
 
 // Each button passes it's content (word) into a function call
 // So there is only one function to actually pass the ingredients to the list (for each category)
+
+//Sauce
+const tomatoSauce = ["Tomato Sauce", 1]
+const cremeFresh = ["Créme Fresh", 1.50]
+
+// Cheese
+const gauda = ["Gauda", 1]
+const emmentaler = ["Emmentaler", 1.50]
+const gorgonzola = ["Gorgonzola", 2]
+
+// Toppings
+const salami = ["Salami", 1]
+const olives = ["Olives", 0.80]
+const ruccola = ["Ruccola", 0.50]
+const parmesan = ["Parmesan", 0.50]
+
+// Extras
+const oregano = ["Oregano", 0.30]
+const garlicOil = ["Garlic Oil", 0.50]
+const chilliOil = ["Chilli Oil", 0.40]
+const rosemaryOil = ["Rosemary Oil", 0.70]
