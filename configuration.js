4233790a35539718db1595f48754556dd217ccce
Simon <simonsch.tz@gmail.com>
2019-12-29 20:47:55 +0100

    Show sum on weg page

--- configuration.js
+++ configuration.js
@@ -4,22 +4,31 @@
 // So there is only one function to actually pass the ingredients to the list (for each category)
 
 //Sauce
-const tomatoSauce = ["Tomato Sauce", 1]
-const cremeFresh = ["Créme Fresh", 1.50]
+const tomatoSauce = ["Tomato Sauce", 1];
+const cremeFresh = ["Créme Fresh", 1.50];
 
 // Cheese
-const gauda = ["Gauda", 1]
-const emmentaler = ["Emmentaler", 1.50]
-const gorgonzola = ["Gorgonzola", 2]
+const gauda = ["Gauda", 1];
+const emmentaler = ["Emmentaler", 1.50];
+const gorgonzola = ["Gorgonzola", 2];
 
 // Toppings
-const salami = ["Salami", 1]
-const olives = ["Olives", 0.80]
-const ruccola = ["Ruccola", 0.50]
-const parmesan = ["Parmesan", 0.50]
+const salami = ["Salami", 1];
+const olives = ["Olives", 0.80];
+const ruccola = ["Ruccola", 0.50];
+const parmesan = ["Parmesan", 0.50];
 
 // Extras
-const oregano = ["Oregano", 0.30]
-const garlicOil = ["Garlic Oil", 0.50]
-const chilliOil = ["Chilli Oil", 0.40]
-const rosemaryOil = ["Rosemary Oil", 0.70]
+const oregano = ["Oregano", 0.30];
+const garlicOil = ["Garlic Oil", 0.50];
+const chilliOil = ["Chilli Oil", 0.40];
+const rosemaryOil = ["Rosemary Oil", 0.70];
+
+//Dough
+const dough = ["Dough", 2];
+
+//
+let sum = 0;
+
+const sumOnPage = document.querySelector('#target-sum');
+sumOnPage.textContent = sum + " €";
