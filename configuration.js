ec8dedbaaed5ef5a45e8198529ae29292e6be8ba
Simon <simonsch.tz@gmail.com>
2020-01-05 16:54:03 +0100

    Add sauce to addIngredient click event

--- configuration.js
+++ configuration.js
@@ -1,4 +1,4 @@
-/**Script to configurate one"s Pizza*/
+/**Script to configurate one's Pizza*/
 
 // Each button passes it"s content (word) into a function call
 // So there is only one function to actually pass the ingredients to the list (for each category)
@@ -6,6 +6,9 @@
 /**********************************
  * Basic Setup of the ingredients
  *********************************/
+
+
+
 //Sauce
 const tomatoSauce = ["Tomato Sauce", 1];
 const cremeFresh = ["Créme Fresh", 1.50];
@@ -32,21 +35,10 @@ let FinalSumExtras = 0;
 
 //Dough
 const dough = ["Dough", 2];
-
 //
 /**********************************
  * Calculating and showing the sum
  *********************************/
-
-//Initialize sum on first load
-let sumPizza = dough[1];
-const sumOnPage = document.querySelector("#target-sum");
-sumOnPage.textContent = sumPizza + " €";
-
-// document.getElementsByName("cheese").onchange = () => {
-//     addCheese()
-// };
-
 const addIngredient = () => {
     /**
      * on call, check which boxes are checked
@@ -55,19 +47,24 @@ const addIngredient = () => {
      * FinalSumX = sumX
      * call summing calculation
      */
+    const checkBoxSauce = document.getElementsByName("sauce");
     const checkBoxCheese = document.getElementsByName("cheese");
     const checkBoxToppings = document.getElementsByName("toppings");
     const checkBoxExtras = document.getElementsByName("extras");
+    let sauce = 0
     let sumCheese = 0;
     let sumToppings = 0;
     let sumExtras = 0;
-    //
+
+    //calculate cheese
     for (let i = 0; i < checkBoxCheese.length; i++) {
         if (checkBoxCheese[i].checked) {
             sumCheese += 2;
         }
     }
 
+    //passing chosen ingredients to outer variables
+    FinalSauce = sauce;
     FinalSumCheese = sumCheese;
     FinalSumToppings = sumToppings;
     FinalSumExtras = sumExtras;
@@ -76,5 +73,8 @@ const addIngredient = () => {
 }
 
 const finalSum = () => {
-    sumOnPage.textContent = sumPizza + sumCheese + sumToppings + sumExtras " €";
+    const sumOnPage = document.querySelector("#target-sum");
+    sumOnPage.textContent = dough[1] + FinalSumCheese + FinalSumToppings + FinalSumExtras + " €";
 }
+//initial call for showing bas ingredients on loading page
+addIngredient();
