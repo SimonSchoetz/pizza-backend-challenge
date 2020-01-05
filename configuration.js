7b90ff667106a1c64bd95cb1dc27d3eae0e53f79
Simon <simonsch.tz@gmail.com>
2020-01-05 17:29:29 +0100

    Refactor cheese to show actual prize

--- configuration.js
+++ configuration.js
@@ -12,26 +12,25 @@
 //Sauce
 const tomatoSauce = ["Tomato Sauce", 1];
 const cremeFresh = ["Créme Fresh", 1.50];
+let finalSauce = 0;
 
 // Cheese
-const gauda = ["Gauda", 1];
-const emmentaler = ["Emmentaler", 1.50];
-const gorgonzola = ["Gorgonzola", 2];
-let FinalSumCheese = 0;
+const cheese = [["Gauda", 1], ["Emmentaler", 1.50], ["Gorgonzola", 2]];
+let finalSumCheese = 0;
 
 // Toppings
 const salami = ["Salami", 1];
 const olives = ["Olives", 0.80];
 const ruccola = ["Ruccola", 0.50];
 const parmesan = ["Parmesan", 0.50];
-let FinalSumToppings = 0;
+let finalSumToppings = 0;
 
 // Extras
 const oregano = ["Oregano", 0.30];
 const garlicOil = ["Garlic Oil", 0.50];
 const chilliOil = ["Chilli Oil", 0.40];
 const rosemaryOil = ["Rosemary Oil", 0.70];
-let FinalSumExtras = 0;
+let finalSumExtras = 0;
 
 //Dough
 const dough = ["Dough", 2];
@@ -51,7 +50,7 @@ const addIngredient = () => {
     const checkBoxCheese = document.getElementsByName("cheese");
     const checkBoxToppings = document.getElementsByName("toppings");
     const checkBoxExtras = document.getElementsByName("extras");
-    let sauce = 0
+    let chosenSauce = 0
     let sumCheese = 0;
     let sumToppings = 0;
     let sumExtras = 0;
@@ -59,22 +58,22 @@ const addIngredient = () => {
     //calculate cheese
     for (let i = 0; i < checkBoxCheese.length; i++) {
         if (checkBoxCheese[i].checked) {
-            sumCheese += 2;
+            sumCheese += cheese[i][1];
         }
     }
 
     //passing chosen ingredients to outer variables
-    FinalSauce = sauce;
-    FinalSumCheese = sumCheese;
-    FinalSumToppings = sumToppings;
-    FinalSumExtras = sumExtras;
+    finalSauce = chosenSauce;
+    finalSumCheese = sumCheese;
+    finalSumToppings = sumToppings;
+    finalSumExtras = sumExtras;
 
     finalSum();
 }
 
 const finalSum = () => {
     const sumOnPage = document.querySelector("#target-sum");
-    sumOnPage.textContent = dough[1] + FinalSumCheese + FinalSumToppings + FinalSumExtras + " €";
+    sumOnPage.textContent = dough[1] + finalSauce + finalSumCheese + finalSumToppings + finalSumExtras + " €";
 }
 //initial call for showing bas ingredients on loading page
 addIngredient();
